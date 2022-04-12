322dc105bde06d6301aaef87833034e17546029a
Nicolas <wellcomenicolas@gmail.com>
2022-04-12 12:56:54 +0200

    Add js-snack-4 FOR EACH

--- js_snack_4/main.js
+++ js_snack_4/main.js
@@ -4,20 +4,36 @@ prendendo alternativamente gli elementi da uno e dall’altro
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 */
 
-const letterArray = ["a","b","c"];
-const numberArray = [1,2,3];
+const letterArray = ["a", "b", "c"];
+const numberArray = [1, 2, 3];
 let newArray = [];
 
-if (letterArray.length == numberArray.length) {
+
+/* CON IL CICLO FOR*/
+/* if (letterArray.length == numberArray.length) {
     for (let i = 0; i < letterArray.length; i++) {
         const elementLetterArray = letterArray[i];
         const elementNumberArray = numberArray[i]
-        newArray.push(elementLetterArray,elementNumberArray)    
+        newArray.push(elementLetterArray, elementNumberArray)
     }
-} else{
+} else {
     alert("I due array non hanno la stessa lunghezza")
 }
 
 
-console.log(newArray);
+console.log(newArray); */
 
+
+/* CON IL CICLO FOR-EACH */
+
+if (letterArray.length == numberArray.length) {
+    letterArray.forEach((element, index) => {
+        newArray.push(element)
+        newArray.push(numberArray[index])
+
+    });
+} else {
+    alert("I due array non hanno la stessa lunghezza")
+}
+
+console.log(newArray);
